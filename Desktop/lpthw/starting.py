4737e647e2846714a0bb23206cab741397aaaec4
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-02-17 15:42:14 -0800

    Exercise 1

---
+++ Desktop/lpthw/starting.py
@@ -0,0 +1,8 @@
+#Exercise 1
+print("Hello, World!")
+print("Hello Again!")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd mych rather you 'not'.")
+print('I "said" do not touch this.')
