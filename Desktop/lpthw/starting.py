0954a16caa0fb6b8239760d09db4848a25ca98b9
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-02-17 16:05:59 -0800

    Exercise 4 - Variables and Names

--- Desktop/lpthw/starting.py
+++ Desktop/lpthw/starting.py
@@ -17,7 +17,7 @@ print("Now I will count the eggs:")
 
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
-print("Is it true that 3 +2 < 5 - 7?")
+print("Is it true that 3 + 2 < 5 - 7?")
 
 print(3 + 2 < 5 - 7)
 
@@ -25,3 +25,20 @@ print("What is 3 + 2?", 3 + 2)
 print("What is 5 - 7?", 5 - 7)
 
 print("Oh, that's why it's False.")
+
+#Exercise 3
+cars = 100
+carspace = 4
+drivers = 30
+passengers = 90
+cars_notdriven = cars - drivers
+driven_cars = drivers
+carpool_capacity = driven_cars * carspace
+avg_passenger_per_car = passengers / driven_cars
+
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_notdriven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about",int(avg_passenger_per_car), "in each car.")
