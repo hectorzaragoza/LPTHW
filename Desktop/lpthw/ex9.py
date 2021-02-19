c13d7c67184afbbbe9be2bec1e5f2d9cf1ac2027
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-02-18 16:40:46 -0800

    Ex 9 - Printing!

---
+++ Desktop/lpthw/ex9.py
@@ -0,0 +1,13 @@
+#Ex 9 Printing
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""There's something going on here.
+with the three double-quotes.
+
+We'll be able to type as much as we like.
+Even 5 lines if we want, 6 or 7.""")
